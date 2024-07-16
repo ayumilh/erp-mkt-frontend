f4232f1179558627c858d0e239d00ba0d0bca544
ayumilh <ayumi.cod06@gmail.com>
2024-07-16 17:49:39 -0300

    rota do autmercado

--- frontend/src/app/authmercado/page.js
+++ frontend/src/app/authmercado/page.js
@@ -1,12 +1,14 @@
+'use client'
 import { redirect } from 'next/navigation';
 import { nextAuthOptions } from '../../app/api/auth/[...nextauth]/route';
 import { getServerSession } from 'next-auth';
 
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 
-export default async function authmercado() {
+
+export default function Authmercado() {
   // const session = await getServerSession(nextAuthOptions)
   // if(!session) {
   //   redirect('/login')
