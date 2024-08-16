6b30536a2dca58aa8825b40e64769afae49dd98b
layumih <ayumi.cod06@gmail.com>
2024-08-16 17:32:36 -0300

    pricing landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,6 +7,7 @@ import {
   FaUsers,
   FaCogs,
 } from "react-icons/fa";
+import CheckIcon from "@mui/icons-material/Check";
 
 export default function Home() {
   return (
@@ -104,8 +105,11 @@ export default function Home() {
         </section>
       </main>
 
-      <section className="h-[500px] w-full bg-gray-100 flex flex-col items-center my-20">
-        <h2 className="text-2xl font-semibold mt-16 mb-8">
+      <section className="h-[460px] w-full bg-gray-100 flex flex-col justify-center items-center my-20">
+        <div className="w-10 mb-3">
+          <hr className="w-full border-segundaria-900 border-[1.5px]" />
+        </div>
+        <h2 className="text-2xl font-semibold mb-8">
           Transforme Desafios em Oportunidades com Nosso Sistema de Dropshipping
         </h2>
         <div className="grid grid-cols-3 gap-4">
@@ -167,6 +171,172 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <article className="w-full flex flex-col justify-center items-center my-20">
+        <div className="w-10 mb-3">
+          <hr className="w-full border-segundaria-900 border-[1.5px]" />
+        </div>
+        <h2 className="text-2xl font-semibold mb-16">
+          Desenvolvendo o plano ideal para o estágio atual do seu negócio
+        </h2>
+        <div className="flex gap-4">
+          <div className="bg-gray-100 rounded-2xl p-6 w-80 h-[500px]">
+            <div className="flex gap-5 items-center">
+              <div className="bg-white rounded-xl p-3">
+                <Image
+                  src="/img/icon-pricing-iniciando.png"
+                  alt="icon-pricing-iniciando"
+                  width={28}
+                  height={28}
+                />
+              </div>
+              <h2 className="text-lg font-semibold">Iniciar</h2>
+            </div>
+            <p className="text-lg font-medium mt-3 mb-6">
+                Transforme seu projeto em realidade com confiança e inovação.
+            </p>
+            <div className="ml-2 py-2">
+              <span className="text-4xl font-semibold">R$120</span>
+              <span className=" text-lg font-light">/mês</span>
+            </div>
+            <ul className="text-lg font-light list-none mt-4 h-32">
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Gestão Básica de Pedidos
+              </li>
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Controle de Estoque
+              </li>
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Integração com Fornecedores
+              </li>
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Relatórios de Vendas
+              </li>
+            </ul>
+            <button className="w-full mt-7 px-3 py-2 bg-segundaria-900 text-white rounded-full hover:bg-segundaria-800 cursor-pointer transition duration-300 ease-in-out">
+              Saiba Mais
+            </button>
+          </div>
+
+          <div
+            className="relative bg-segundaria-900 rounded-2xl p-6 w-80 h-[500px]"
+            style={{ top: "-35px" }}
+            >
+            <span className="absolute top-0 right-0 bg-gray-100 text-segundaria-900 text-xs font-semibold px-2 py-1 rounded-bl-md rounded-tr-lg">
+                Popular
+            </span>
+            <div className="flex gap-5 items-center">
+                <div className="bg-white rounded-xl p-3">
+                <Image
+                    src="/img/icon-pricing-evoluindo.png"
+                    alt="icon-pricing-evoluindo"
+                    width={28}
+                    height={28}
+                />
+                </div>
+                <h2 className="text-lg font-semibold text-white">Evoluindo</h2>
+            </div>
+            <p className="text-white text-lg font-light mt-3 mb-6">
+                Seu sucesso começa aqui e transforme sua jornada.
+            </p>
+            <div className="ml-2 py-2">
+                <span className="text-white text-4xl font-semibold">R$190</span>
+                <span className="text-white text-lg font-light">/mês</span>
+            </div>
+            <ul className="text-white text-lg font-light list-none mt-4">
+                <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                    <CheckIcon fontSize="small" className="text-segundaria-900" />
+                </div>
+                Automatização de Pedidos
+                </li>
+                <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                    <CheckIcon fontSize="small" className="text-segundaria-900" />
+                </div>
+                Gestão de Inventário em Tempo Real
+                </li>
+                <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                    <CheckIcon fontSize="small" className="text-segundaria-900" />
+                </div>
+                Integração com Múltiplas Plataformas
+                </li>
+                <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                    <CheckIcon fontSize="small" className="text-segundaria-900" />
+                </div>
+                Análise de Dados Avançada
+                </li>
+            </ul>
+            <button className="w-full mt-7 px-3 py-2 bg-white text-segundaria-900 rounded-full hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out">
+                Saiba Mais
+            </button>
+            </div>
+
+          <div className="bg-gray-100 rounded-2xl p-6 w-80 h-[500px]">
+            <div className="flex gap-5 items-center">
+              <div className="bg-white rounded-xl p-3">
+                <Image
+                  src="/img/icon-pricing-potencializando.png"
+                  alt="icon-pricing-potencializando"
+                  width={28}
+                  height={28}
+                />
+              </div>
+              <h2 className="text-lg font-semibold">Potencializando</h2>
+            </div>
+            <p className="text-lg font-medium mt-3 mb-6">
+              Alcance excelentes resultados de forma simples e eficiente.
+            </p>
+            <div className="ml-2 py-2">
+              <span className="text-4xl font-semibold">R$240</span>
+              <span className=" text-lg font-light">/mês</span>
+            </div>
+            <ul className="text-lg font-light list-none mt-4 h-32">
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Automação Avançada
+              </li>
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Análise de Dados em Tempo Real
+              </li>
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Suporte Prioritário
+              </li>
+              <li className="flex items-center mb-2 text-sm font-light">
+                <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                  <CheckIcon fontSize="small" className="text-white" />
+                </div>
+                Personalização e Escalabilidade
+              </li>
+            </ul>
+            <button className="w-full mt-7 px-3 py-2 bg-segundaria-900 text-white rounded-full hover:bg-segundaria-800 cursor-pointer transition duration-300 ease-in-out">
+              Saiba Mais
+            </button>
+          </div>
+        </div>
+      </article>
     </div>
   );
 }
