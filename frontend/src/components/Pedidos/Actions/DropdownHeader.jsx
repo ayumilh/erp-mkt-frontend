8ee40784aec8b54b488d3bc9ad37ea5a7e0f1475
layumih <ayumi.cod06@gmail.com>
2024-08-26 17:36:25 -0300

    commit

--- frontend/src/components/Pedidos/Actions/DropdownHeader.jsx
+++ frontend/src/components/Pedidos/Actions/DropdownHeader.jsx
@@ -1,96 +1,111 @@
 'use client'
 import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export const DropdownHeader = ({ setActiveTable }) => {
-	const [isOpenLista, setIsOpenLista] = useState(false);
-	const [currentText, setCurrentText] = useState('Pedidos');
-	const dropdownPedidosRef = useRef(null);
+    const [isOpenLista, setIsOpenLista] = useState(false);
+    const [currentText, setCurrentText] = useState('Pedidos');
+    const [orderCounts, setOrderCounts] = useState({});
+    const dropdownPedidosRef = useRef(null);
 
-	const handleClickLista = () => {
-		setIsOpenLista(!isOpenLista);
-	};
+    const handleClickLista = () => {
+        setIsOpenLista(!isOpenLista);
+    };
 
-	const handleClickEmitir = (text) => {
-		setActiveTable(text);
-		setCurrentText(text);
-	};
+    const handleClickEmitir = (text) => {
+        setActiveTable(text);
+        setCurrentText(text);
+    };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownPedidosRef.current && !dropdownPedidosRef.current.contains(event.target)) {
+                setIsOpenLista(false);
+            }
+        }
 
-	useEffect(() => {
-		const handleClickOutside = (event) => {
-			if (dropdownPedidosRef.current && !dropdownPedidosRef.current.contains(event.target)) {
-				setIsOpenLista(false);
-			}
-		}
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    }, [dropdownPedidosRef]);
 
-		document.addEventListener("mousedown", handleClickOutside);
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		}
-	}, [dropdownPedidosRef])
+    useEffect(() => {
+        const fetchOrderCounts = async () => {
+            try {
+                const response = await axios.get('https://erp-mkt.vercel.app/api/mercadolivre/count-orders');
+				console.log('response', response.data);
+                setOrderCounts(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar a quantidade de pedidos:', error);
+            }
+        };
 
-	return (
-		<div className="relative inline-block text-left z-50" ref={dropdownPedidosRef}>
-			<div>
-				<button
-					type="button"
-					className="flex items-center justify-center rounded-lg px-2 md:px-3"
-					id="options-menu"
-					aria-haspopup="true"
-					aria-expanded="true"
-					onClick={handleClickLista}
-				>
-					<span className="hover:text-black font-medium text-sm md:text-base">{currentText}</span>
-					<KeyboardArrowDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-				</button>
-				<hr className="border-segundaria-900 border-[1.5px]" />
-			</div>
+        fetchOrderCounts();
+    }, []);
 
-			{isOpenLista && (
-				<div className="origin-top-center absolute mt-2 px-2 rounded-md bg-primaria-900">
-					<div className="w-28 my-2" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-						<button
-							className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white ho rounded-smver:bg-opacity-80"
-							role="menuitem"
-							onClick={() => handleClickEmitir('Pedidos')}
-						>
-							<span>Pedidos</span>
-							<span className='font-medium text-neutral-600 opacity-90'>0</span>
-						</button>
-						<button
-							className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white ho rounded-smver:bg-opacity-80"
-							role="menuitem"
-							onClick={() => handleClickEmitir('Emitir')}
-						>
-							<span>Emitir</span> 
-							<span className='font-medium text-neutral-600 opacity-90'>0</span>
-						</button>
-						<button className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
-							<span>Enviar</span> 
-							<span className='font-medium text-neutral-600 opacity-90'>0</span>
-						</button>
-						<button
-							onClick={() => handleClickEmitir('Imprimir')}
-							className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
-							<span>Imprimir</span>
-							<span className='font-medium text-neutral-600 opacity-90'>0</span>
-						</button>
-						<button
-							onClick={() => handleClickEmitir('Retirada')}
-							className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
-							<span>Retirada</span> 
-							<span className='font-medium text-neutral-600 opacity-90'>0</span>
-						</button>
-						<button
-							onClick={() => handleClickEmitir('Enviados')}
-							className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
-							<span>Enviados</span> 
-							<span className='font-medium text-neutral-600 opacity-90'>0</span>
-						</button>
-					</div>
-				</div>
-			)}
-		</div>
-	)
+    return (
+        <div className="relative inline-block text-left z-50" ref={dropdownPedidosRef}>
+            <div>
+                <button
+                    type="button"
+                    className="flex items-center justify-center rounded-lg px-2 md:px-3"
+                    id="options-menu"
+                    aria-haspopup="true"
+                    aria-expanded="true"
+                    onClick={handleClickLista}
+                >
+                    <span className="hover:text-black font-medium text-sm md:text-base">{currentText}</span>
+                    <KeyboardArrowDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                </button>
+                <hr className="border-segundaria-900 border-[1.5px]" />
+            </div>
+
+            {isOpenLista && (
+                <div className="origin-top-center absolute mt-2 px-2 rounded-md bg-primaria-900">
+                    <div className="w-28 my-2" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                        <button
+                            className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm"
+                            role="menuitem"
+                            onClick={() => handleClickEmitir('Pedidos')}
+                        >
+                            <span>Pedidos</span>
+                            <span className='font-medium text-neutral-600 opacity-90'>{orderCounts.Pedidos || 0}</span>
+                        </button>
+                        <button
+                            className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm"
+                            role="menuitem"
+                            onClick={() => handleClickEmitir('Emitir')}
+                        >
+                            <span>Emitir</span> 
+                            <span className='font-medium text-neutral-600 opacity-90'>{orderCounts.Emitir || 0}</span>
+                        </button>
+                        <button className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
+                            <span>Enviar</span> 
+                            <span className='font-medium text-neutral-600 opacity-90'>{orderCounts.Enviar || 0}</span>
+                        </button>
+                        <button
+                            onClick={() => handleClickEmitir('Imprimir')}
+                            className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
+                            <span>Imprimir</span>
+                            <span className='font-medium text-neutral-600 opacity-90'>{orderCounts.Imprimir || 0}</span>
+                        </button>
+                        <button
+                            onClick={() => handleClickEmitir('Retirada')}
+                            className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
+                            <span>Retirada</span> 
+                            <span className='font-medium text-neutral-600 opacity-90'>{orderCounts.Retirada || 0}</span>
+                        </button>
+                        <button
+                            onClick={() => handleClickEmitir('Enviados')}
+                            className="flex justify-between w-full text-sm font-medium px-2 py-1 hover:text-black hover:bg-white hover:bg-opacity-80 rounded-sm" role="menuitem">
+                            <span>Enviados</span> 
+                            <span className='font-medium text-neutral-600 opacity-90'>{orderCounts.Enviados || 0}</span>
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
