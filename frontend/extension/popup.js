415a6a369d1e21acb443ecad729a3b27bcb2fe25
layumih <ayumi.cod06@gmail.com>
2024-09-03 09:51:40 -0300

    erro no require do puppeteer

--- frontend/extension/popup.js
+++ frontend/extension/popup.js
@@ -1,4 +1,15 @@
-document.getElementById('botao').addEventListener('click', () => {
+const puppeteer = require('puppeteer');
+
+document.getElementById('botao').addEventListener('click', function() {
   const link = document.getElementById('link').value;
-  console.log('Link:', link);
-});
+  const firstSlashIndex = link.indexOf('/');
+  const extractedLink = link.substring(firstSlashIndex);
+
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.google.com${extractedLink}`);
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
+})});
--- frontend/src/components/Auth/Cadastro/HeaderForm.jsx
+++ frontend/src/components/Auth/Cadastro/HeaderForm.jsx
@@ -1,8 +1,8 @@
 const HeaderForm = () => {
     return (
-        <div className="form-title flex flex-col items-center">
+        <div className="form-title flex flex-col">
             <h1 className="text-2xl md:text-3xl text-neutral-700 font-medium tracking-wider">Junte-se a nós</h1>
-            <h2 className="text-3xl md:text-2xl text-neutral-700 font-medium mt-2">Bem-vindo ao <span className="text-segundaria-900 font-semibold">SIMK</span></h2>
+            <h2 className="text-3xl md:text-2xl text-neutral-700 font-medium mt-1">Bem-vindo ao <span className="text-segundaria-900 font-semibold">SIMK</span></h2>
         </div>
     )
 }
