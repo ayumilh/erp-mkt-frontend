26f7be391bb316e5e58a4f1b2616a2ed0d7b9f67
layumih <ayumi.cod06@gmail.com>
2024-09-18 10:52:24 -0300

    gsap na lading page feito

---
+++ frontend/src/components/LandingPage/Footer.jsx
@@ -0,0 +1,92 @@
+import TelegramIcon from '@mui/icons-material/Telegram';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+const Footer = () => {
+    return (
+        <footer className="w-full h-[440px] bg-segundaria-900 flex justify-between items-center gap-20 px-16 py-20 rounded-t-2xl">
+            <div className="w-1/2 flex flex-col items-center">
+                <div className="flex flex-col gap-3">
+                    <h4 className="text-white font-medium text-2xl">Ainda ficou com duvida?</h4>
+                    <span className="text-white">Preencha sua dúvida no campo ao lado e nosso consultor entrará em contato.</span>
+                    <div className="relative w-80">
+                        <input type="text" className="w-full h-10 rounded-2xl px-4 py-2 bg-transparent ring-1 ring-gray-100 pr-10" placeholder="" />
+                        <ArrowForwardIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer" />
+                    </div>
+                    <div className="flex gap-3">
+                        <TelegramIcon className="text-white cursor-pointer" />
+                        <InstagramIcon className="text-white cursor-pointer" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-1/2 flex flex-wrap items-center">
+                <div className="w-1/2 flex flex-col mt-8">
+                    <h2 className="text-white opacity-50 uppercase text-sm">Suporte</h2>
+                    <nav className="flex flex-col gap-2 mt-4">
+                        <a href="#" className="w-52 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Grupo no Telegram
+                        </a>
+                        <a href="#" className="w-52 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Central de Ajuda
+                        </a>
+                        <a href="#" className="w-52 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Termo e condições
+                        </a>
+                        <a href="#" className="w-52 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Política de privacidade
+                        </a>
+                    </nav>
+                </div>
+                <div className="w-1/2 flex flex-col mt-8">
+                    <h2 className="text-white opacity-50 uppercase text-sm">Empresa</h2>
+                    <nav className="flex flex-col gap-2 mt-4">
+                        <a href="#" className="w-20  text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Sobre
+                        </a>
+                        <a href="#" className="w-20 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Blog
+                        </a>
+                        <a href="#" className="w-20 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Carreiras
+                        </a>
+                    </nav>
+                </div>
+                <div className="w-1/2 flex flex-col mt-8">
+                    <h2 className="text-white opacity-50 uppercase text-sm">Contato</h2>
+                    <nav className="flex flex-col gap-2 mt-4">
+                        <a href="#" className="w-40 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Fale Conosco
+                        </a>
+                        <a href="#" className="w-40 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Suporte
+                        </a>
+                        <a href="#" className="w-40 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            FAQ
+                        </a>
+                    </nav>
+                </div>
+                <div className="w-1/2 flex flex-col mt-8">
+                    <h2 className="text-white font-light opacity-50 uppercase">Integrações</h2>
+                    <nav className="flex flex-col gap-2 mt-4">
+                        <a href="#" className="w-32 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Shopee
+                        </a>
+                        <a href="#" className="w-32 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Mercado Livre
+                        </a>
+                        <a href="#" className="w-32 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Amazon
+                        </a>
+                        <a href="#" className="w-32 text-white opacity-80 hover:opacity-100 transition duration-300 ease-in-out hover:border-b hover:border-white">
+                            Magalu
+                        </a>
+                    </nav>
+                </div>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/LandingPage/PlansAndPrices.jsx
@@ -0,0 +1,251 @@
+'use client'
+import { useEffect } from "react";
+import CheckIcon from "@mui/icons-material/Check";
+import Image from "next/image";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const PlansAndPrices = () => {
+    useEffect(() => {
+        gsap.fromTo(".animate-title-plans", 
+            {
+                opacity: 0,
+                y: -10
+            }, 
+            {
+                opacity: 1,
+                y: 0,
+                duration: 0.6,
+                ease: "power2.out",
+                stagger: null,
+                scrollTrigger: {
+                    trigger: ".animate-tool-div",
+                    start: "top 350px",
+                    end: "bottom 500px",
+                },
+            }
+        );
+
+        gsap.fromTo(".animate-content-bg", 
+            {
+                opacity: 0,
+                y: -50,
+            },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 0.6,
+                ease: "power2.out",
+                stagger: null,
+                scrollTrigger: {
+                    trigger: ".animate-content-bg",
+                    start: "top 680px",
+                    end: "bottom 800px",
+                },
+            }
+        );
+
+        gsap.fromTo(".animate-title-plans", 
+            {
+                opacity: 0,
+                y: -10
+            }, 
+            {
+                opacity: 1,
+                y: 0,
+                duration: 2,
+                ease: "power1",
+            }
+        );
+
+        gsap.fromTo(".animate-text-plans", 
+            {
+                opacity: 0,
+                y: -10
+            }, 
+            {
+                opacity: 1,
+                y: 0,
+                duration: 0.6,
+                ease: "power1",
+                stagger: 0.05,
+                scrollTrigger: {
+                    trigger: ".animate-content-bg",
+                    start: "top 640px",
+                    end: "bottom 500px",
+                },
+            }
+        );
+    }, []);
+
+    return (
+        <article className="w-full flex flex-col justify-center items-center my-20">
+            <div className="w-10 mb-3">
+                <hr className="w-full border-segundaria-900 border-[1.5px]" />
+            </div>
+            <h2 className="animate-title-plans text-2xl font-semibold mb-16">
+                Desenvolvendo o plano ideal para o estágio atual do seu negócio
+            </h2>
+            <div className="flex gap-4">
+                <div className="animate-content-bg bg-gray-100 rounded-2xl p-6 w-80 h-[500px]">
+                    <div className="flex gap-5 items-center">
+                        <div className="bg-white rounded-xl p-3">
+                            <Image
+                                src="/img/landingPage/icon-pricing-iniciando.png"
+                                alt="icon-pricing-iniciando"
+                                width={28}
+                                height={28}
+                            />
+                        </div>
+                        <h2 className="text-lg font-semibold">Iniciar</h2>
+                    </div>
+                    <p className="animate-text-plans text-lg font-medium mt-3 mb-6">
+                        Transforme seu projeto em realidade com confiança e inovação.
+                    </p>
+                    <div className="animate-text-plans ml-2 py-2">
+                        <span className="text-4xl font-semibold">R$120</span>
+                        <span className=" text-lg font-light">/mês</span>
+                    </div>
+                    <ul className="animate-text-plans text-lg font-light list-none mt-4 h-32">
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Gestão Básica de Pedidos
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Controle de Estoque
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Integração com Fornecedores
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Relatórios de Vendas
+                        </li>
+                    </ul>
+                    <button className="animate-title-plans w-full mt-7 px-3 py-2 bg-segundaria-900 text-white rounded-full hover:bg-segundaria-800 cursor-pointer transition duration-300 ease-in-out">
+                        Saiba Mais
+                    </button>
+                </div>
+
+                <div
+                    className="animate-content-bg relative bg-segundaria-900 rounded-2xl p-6 w-80 h-[500px]"
+                    style={{ top: "-35px" }}
+                >
+                    <span className="absolute top-0 right-0 bg-gray-100 text-segundaria-900 text-xs font-semibold px-2 py-1 rounded-bl-md rounded-tr-lg">
+                        Popular
+                    </span>
+                    <div className="flex gap-5 items-center">
+                        <div className="bg-white rounded-xl p-3">
+                            <Image
+                                src="/img/landingPage/icon-pricing-evoluindo.png"
+                                alt="icon-pricing-evoluindo"
+                                width={28}
+                                height={28}
+                            />
+                        </div>
+                        <h2 className="text-lg font-semibold text-white">Evoluindo</h2>
+                    </div>
+                    <p className="animate-text-plans text-white text-lg font-light mt-3 mb-6">
+                        Seu sucesso começa aqui e transforme sua jornada.
+                    </p>
+                    <div className="animate-text-plans ml-2 py-2">
+                        <span className="text-white text-4xl font-semibold">R$190</span>
+                        <span className="text-white text-lg font-light">/mês</span>
+                    </div>
+                    <ul className="animate-text-plans text-white text-lg font-light list-none mt-4">
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-segundaria-900" />
+                            </div>
+                            Automatização de Pedidos
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-segundaria-900" />
+                            </div>
+                            Gestão de Inventário em Tempo Real
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-segundaria-900" />
+                            </div>
+                            Integração com Múltiplas Plataformas
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-white rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-segundaria-900" />
+                            </div>
+                            Análise de Dados Avançada
+                        </li>
+                    </ul>
+                    <button className="animate-title-plans w-full mt-7 px-3 py-2 bg-white text-segundaria-900 rounded-full hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out">
+                        Saiba Mais
+                    </button>
+                </div>
+
+                <div className="animate-content-bg bg-gray-100 rounded-2xl p-6 w-80 h-[500px]">
+                    <div className="flex gap-5 items-center">
+                        <div className="bg-white rounded-xl p-3">
+                            <Image
+                                src="/img/landingPage/icon-pricing-potencializando.png"
+                                alt="icon-pricing-potencializando"
+                                width={28}
+                                height={28}
+                            />
+                        </div>
+                        <h2 className="text-lg font-semibold">Potencializando</h2>
+                    </div>
+                    <p className="animate-text-plans text-lg font-medium mt-3 mb-6">
+                        Alcance excelentes resultados de forma simples e eficiente.
+                    </p>
+                    <div className="animate-text-plans ml-2 py-2">
+                        <span className="text-4xl font-semibold">R$240</span>
+                        <span className=" text-lg font-light">/mês</span>
+                    </div>
+                    <ul className="animate-text-plans text-lg font-light list-none mt-4 h-32">
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Automação Avançada
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Análise de Dados em Tempo Real
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Suporte Prioritário
+                        </li>
+                        <li className="flex items-center mb-2 text-sm font-light">
+                            <div className="bg-segundaria-900 rounded-full w-6 h-6 flex items-center justify-center p-1 mr-2">
+                                <CheckIcon fontSize="small" className="text-white" />
+                            </div>
+                            Personalização e Escalabilidade
+                        </li>
+                    </ul>
+                    <button className="animate-title-plans w-full mt-7 px-3 py-2 bg-segundaria-900 text-white rounded-full hover:bg-segundaria-800 cursor-pointer transition duration-300 ease-in-out">
+                        Saiba Mais
+                    </button>
+                </div>
+            </div>
+        </article>
+    )
+}
+export default PlansAndPrices;
