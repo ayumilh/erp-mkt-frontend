cc490bd19107c80a58712fecd3b08621d26133bd
layumih <ayumi.cod06@gmail.com>
2024-09-27 10:32:08 -0300

    toggleTheme no mobile

--- frontend/src/components/Geral/Button/ToggleTheme.jsx
+++ frontend/src/components/Geral/Button/ToggleTheme.jsx
@@ -7,11 +7,11 @@ function ToggleTheme() {
     const { theme, setTheme } = useContext(ThemeContext)
 
     return (
-        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className='flex items-center group px-2 py-1'>
+        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className='flex h-12 lg:h-auto items-center group px-4 lg:px-2 py-1'>
             {theme === 'light' ?
-                <LightModeIcon sx={{ width: '16px' }} className='text-neutral-700 dark:text-gray-300 group-hover:text-segundaria-900' /> : <DarkModeIcon sx={{ width: '16px' }} className='text-neutral-700 dark:text-gray-300 group-hover:text-segundaria-900' />
+                <LightModeIcon className='w-8 lg:w-4 text-neutral-700 dark:text-gray-300 group-hover:text-segundaria-900' /> : <DarkModeIcon className='w-8 lg:w-4 text-neutral-700 dark:text-gray-300 group-hover:text-segundaria-900' />
             }
-            <span className='text-sm dark:text-gray-200 group-hover:text-segundaria-900 font-medium transition duration-300 ease-out ml-2'>
+            <span className='lg:text-sm dark:text-gray-200 group-hover:text-segundaria-900 font-medium transition duration-300 ease-out ml-2'>
                 {theme === 'dark' ? 'Modo escuro' : 'Modo claro'}
             </span>
         </button>
