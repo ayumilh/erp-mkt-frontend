fcc4cb1ae684c65d19905f9d5f95aa3b12c47527
layumih <ayumi.cod06@gmail.com>
2024-09-30 17:09:45 -0300

    bug no tailwind

--- frontend/src/components/Dashboard/PieAnalysis/PieRankingChart.jsx
+++ frontend/src/components/Dashboard/PieAnalysis/PieRankingChart.jsx
@@ -1,16 +1,20 @@
 'use client'
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import { ThemeContext } from '../../../contexts/ThemeContext';
 
 const PieRankingChart = () => {
+  const { theme } = useContext(ThemeContext);
+
   const [chartData, setChartData] = useState({
     series: [35, 58, 23],
     options: {
       chart: {
         width: 380,
         type: 'polarArea',
+        background: 'transparent',
       },
-      colors: ['#624DE3', '#8574FF', '#4B37B2' ],
+      colors: ['#624DE3', '#8574FF', '#4B37B2'],
       labels: ['Rose A', 'Rose B', 'Rose C'],
       fill: {
         opacity: 1
@@ -35,8 +39,18 @@ const PieRankingChart = () => {
           },
         }
       },
+      theme: {
+        mode: theme === 'dark' ? 'dark' : 'light',
+        // palette: 'palette1',
+        monochrome: {
+          enabled: false,
+          color: theme === 'dark' ? '#ffffff' : '#000000',
+          shadeTo: 'light',
+          shadeIntensity: 0.65
+        }
+      }
     }
-  })
+  });
 
   return (
     <div>
