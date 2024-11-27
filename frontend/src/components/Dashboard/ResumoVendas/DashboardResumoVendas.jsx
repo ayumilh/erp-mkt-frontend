76cc91bbfa89b36faec48b9575bd3d1dca0dd79a
layumih <ayumi.cod06@gmail.com>
2024-11-27 16:57:43 -0300

    hidden do resumo da dashboard

--- frontend/src/components/Dashboard/ResumoVendas/DashboardResumoVendas.jsx
+++ frontend/src/components/Dashboard/ResumoVendas/DashboardResumoVendas.jsx
@@ -3,17 +3,21 @@ import { useEffect, useState, useCallback } from 'react';
 import dynamic from "next/dynamic";
 import axios from 'axios';
 import { searchUserId } from '@/utils/searchUserId';
+import { IconButton } from "@mui/material"
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const ChartPie2 = dynamic(() => import('./ChartPie2'), { ssr: false });
 
 export const DashboardResumoVendas = () => {
+    const [showPassword, setShowPassword] = useState(false)
     const [data, setData] = useState([]);
     const userId = searchUserId();
 
     const handleButtonClick = useCallback(async () => {
         try {
             await axios.post('https://erp-mkt.vercel.app/api/mercadolivre/item-visits', {
-                userId: userId 
+                userId: userId
             });
         } catch (error) {
             console.error(`Error: ${error}`);
@@ -26,8 +30,8 @@ export const DashboardResumoVendas = () => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('https://erp-mkt.vercel.app/api/mercadolivre/visits', {
-                    params: { userId } 
-                });    
+                    params: { userId }
+                });
                 const restructuredData = response.data.visits.map((data) => {
                     return {
                         conversion_rate: data.conversion_rate,
@@ -42,6 +46,10 @@ export const DashboardResumoVendas = () => {
         fetchData();
     }, [userId, handleButtonClick]);
 
+
+    const handleClickShowPassword = () => (
+        setShowPassword(!showPassword)
+    )
 
     const firstElement = data.length > 0 ? data[0] : { conversion_rate: 0, total_visits: 0 };
 
@@ -91,51 +99,62 @@ export const DashboardResumoVendas = () => {
             <hr className='w-1 h-full rounded-full bg-gray-200 mx-4 hidden xl:block' />
 
             <div className='w-full xl:w-1/2 flex'>
-                <div className='w-full flex flex-col md:flex-row justify-around gap-4'>
-                    {/* conta a receber */}
-                    <div className='w-full xl:w-64 px-4 py-2 flex flex-col items-center text-center'>
-                        <div className='w-full flex gap-2'>
-                            <span className='text-green-500 font-semibold text-sm'>0</span>
-                            <span className='text-neutral-600 dark:text-gray-200 font-semibold text-sm'>Contas a receber</span>
-                        </div>
-                        <div className='w-full flex justify-between py-1'>
-                            <span className='w-2/3 text-neutral-500 dark:text-gray-300 font-medium text-start text-sm'>Total</span>
-                            <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-end text-sm'>0</span>
-                        </div>
-                        <div className='w-full flex justify-between py-1'>
-                            <span className='w-2/3 text-neutral-500 dark:text-gray-300 font-medium text-start text-sm'>Taxas</span>
-                            <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-end text-sm'>0</span>
-                        </div>
-
-                        <hr className='w-full border border-gray-200 my-2' />
-
-                        <div className='w-full flex justify-between'>
-                            <span className='w-2/3 text-green-500 text-start text-sm'>Liquido</span>
-                            <span className='w-1/3 text-neutral-600 dark:text-gray-200 font-medium text-end text-sm'>0</span>
-                        </div>
+                <div className='w-full flex flex-col'>
+                    <div className="relative flex items-center justify-end">
+                        <IconButton
+                            onClick={handleClickShowPassword}
+                            aria-label="toggle password visibility"
+                        >
+                            {showPassword ? <Visibility className='dark:text-gray-200 text-neutral-700' /> : <VisibilityOff className='dark:text-gray-200 text-neutral-700' />}
+                        </IconButton>
                     </div>
-                    {/* conta a pagar */}
-                    <div className='w-full xl:w-64 h-full px-4 py-2 flex flex-col items-center text-center'>
-                        <div className='w-full h-1/2'>
-                            <div className='flex gap-2'>
-                                <span className='text-red-500 font-semibold text-sm'>0</span>
-                                <span className='text-neutral-600 dark:text-gray-200 font-semibold text-sm'>Contas a pagar</span>
+
+                    <div className='flex flex-col md:flex-row justify-around gap-4'>
+                        {/* conta a receber */}
+                        <div className='w-full xl:w-64 px-4 py-2 flex flex-col items-center text-center'>
+                            <div className='w-full flex gap-2'>
+                                <span className='text-green-500 font-semibold text-sm'>0</span>
+                                <span className='text-neutral-600 dark:text-gray-200 font-semibold text-sm'>Contas a receber</span>
                             </div>
-                            <div className='w-full flex justify-between pt-1'>
-                                <span className='w-2/3 text-neutral-500 dark:text-gray-300 font-medium text-start text-sm'>Valor Total a pagar</span>
-                                <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-end text-sm'>0</span>
+                            <div className='w-full flex justify-between pt-2 pb-1'>
+                                <span className='w-2/3 text-neutral-500 dark:text-gray-300 font-medium text-start text-sm'>Total</span>
+                                <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-end text-sm'>{showPassword ? '0' : '****'}</span>
+                            </div>
+                            <div className='w-full flex justify-between py-1'>
+                                <span className='w-2/3 text-neutral-500 dark:text-gray-300 font-medium text-start text-sm'>Taxas</span>
+                                <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-end text-sm'>{showPassword ? '0' : '****'}</span>
+                            </div>
+
+                            <hr className='w-full border border-gray-200 my-2' />
+
+                            <div className='w-full flex justify-between'>
+                                <span className='w-2/3 text-green-500 text-start text-sm'>Liquido</span>
+                                <span className='w-1/3 text-neutral-600 dark:text-gray-200 font-medium text-end text-sm'>{showPassword ? '0' : '****'}</span>
                             </div>
                         </div>
-                        
-                        <hr className='w-full border border-gray-100 mt-2 mb-2 xl:mt-0 xl:mb-4' />
-
-                        <div className='w-full h-1/2'>
-                            <div className='flex'>
-                                <span className='text-neutral-600 dark:text-gray-200 font-semibold text-sm'>Notas fiscais</span>
+                        {/* conta a pagar */}
+                        <div className='w-full xl:w-64 h-full px-4 py-2 flex flex-col items-center text-center'>
+                            <div className='w-full h-1/2'>
+                                <div className='flex gap-2'>
+                                    <span className='text-red-500 font-semibold text-sm'>0</span>
+                                    <span className='text-neutral-600 dark:text-gray-200 font-semibold text-sm'>Contas a pagar</span>
+                                </div>
+                                <div className='w-full flex justify-between pt-2'>
+                                    <span className='w-2/3 text-neutral-500 dark:text-gray-300 font-medium text-start text-sm'>Valor Total a pagar</span>
+                                    <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-end text-sm'>{showPassword ? '0' : '****'}</span>
+                                </div>
                             </div>
-                            <div className='w-full text-start py-1'>
-                                <span className='w-2/3 text-green-500 font-medium text-sm mr-3'>0</span>
-                                <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-sm'>Novas NF-es</span>
+
+                            <hr className='w-full border border-gray-100 mt-2 mb-2 xl:mt-4 xl:mb-4' />
+
+                            <div className='w-full h-1/2'>
+                                <div className='flex'>
+                                    <span className='text-neutral-600 dark:text-gray-200 font-semibold text-sm'>Notas fiscais</span>
+                                </div>
+                                <div className='w-full text-start py-1'>
+                                    <span className='w-2/3 text-green-500 font-medium text-sm mr-3'>0</span>
+                                    <span className='w-1/3 text-neutral-500 dark:text-gray-300 font-medium text-sm'>Novas NF-es</span>
+                                </div>
                             </div>
                         </div>
                     </div>
