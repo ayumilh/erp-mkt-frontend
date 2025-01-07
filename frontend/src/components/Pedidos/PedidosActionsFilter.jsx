0f082f504048f72157918a9a2ef7b9191ed4004e
layumih <ayumi.cod06@gmail.com>
2025-01-06 23:29:57 -0300

    include do estrutura do filtro de horario

--- frontend/src/components/Pedidos/PedidosActionsFilter.jsx
+++ frontend/src/components/Pedidos/PedidosActionsFilter.jsx
@@ -38,7 +38,33 @@ const PedidosActionsFilter = ({ setSearchTerm, searchTerm = '', searchColumn, se
           </div>
         </div>
 
-        <div className="flex w-full mt-4 md:mt-0">
+        <div className='flex w-full items-center'>
+          <div className="flex w-3/6 md:w-4/6 lg:w-3/6">
+            <select
+              title='Coluna de Pesquisa'
+              className="dark:bg-neutral-800 w-full dark:text-gray-200 h-9 rounded-l text-xs md:text-sm font-normal py-2 pl-2 pr-4"
+              value={searchColumn}
+              onChange={(e) => setSearchColumn(e.target.value)}
+            >
+              <option value="hr_pedido" title='hr do pedido'>Hora do pedido</option>
+              <option value="hr_entrega" title='hr de entrega'>Hora da entrega</option>
+              <option value="hr_envio" title='hr de envio'>Hora da envio</option>
+            </select>
+          </div>
+
+          <div className="flex w-4/6 md:w-2/6 lg:w-3/6">
+            <input
+              title='Pesquise por data'
+              className="w-full dark:bg-neutral-800 dark:text-gray-200 h-9 rounded-r overflow-hidden text-xs md:text-sm font-normal px-3 py-2 placeholder:italic"
+              style={{ textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+              type="date"
+              value={searchTerm || ''}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+        </div>
+
+        {/* <div className="flex w-full mt-4 md:mt-0">
           <div className='flex w-1/2'>
             <select title='Número da estante' className="rounded dark:bg-neutral-800 dark:text-gray-200 px-3 py-2 mr-2 w-full text-xs md:text-sm text-neutral-800 font-normal" name="nEstante" id="nEstante">
               <option className="text-sm text-neutral-800 dark:text-gray-200 font-normal" value="nEstante">N° da Estante</option>
@@ -54,7 +80,7 @@ const PedidosActionsFilter = ({ setSearchTerm, searchTerm = '', searchColumn, se
               <option className="text-sm text-neutral-800 dark:text-gray-200 font-normal" value="Caraguatatuba">Caraguatatuba</option>
             </select>
           </div>
-        </div>
+        </div> */}
 
         <div className="flex justify-between mt-4 md:mt-0 md:ml-2 2xl:ml-5">
           <BtnActive title="Pesquisar" size="btnSimple" />
