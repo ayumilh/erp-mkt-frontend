876f1305d043006b3a838f9bd94f84e386f565b1
layumih <ayumi.cod06@gmail.com>
2025-01-08 18:54:50 -0300

    tratando erros da pagina authmercado

--- frontend/src/app/authmercado/page.js
+++ frontend/src/app/authmercado/page.js
@@ -1,13 +1,21 @@
 'use client'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Image from 'next/image';
+import SidebarContent from "@/components/Drawer/desktop/SidebarContent";
+import ProdutosContent from "@/components/Produtos/ProdutosContent";
 import { useRouter, useSearchParams } from 'next/navigation';
 import { searchUserId } from '@/utils/searchUserId';
 import SuccessNotification from '@/components/Geral/Notifications/SuccessNotification';
 import ErrorNotification from '@/components/Geral/Notifications/ErrorNotification';
+import HamburgerContent from '@/components/Drawer/mobile/HamburgerContent';
+import ActionsContent from '@/components/Config/ActionsConfig/ActionsContent';
+import TitlePage from '@/components/Geral/TitlePage';
+
 
 export default function Authmercado() {
   const [statusRequestCodeMercado, setStatusRequestCodeMercado] = useState(null);
+  const [modalMessage, setModalMessage] = useState('');
   const router = useRouter();
   const [userId, setUserId] = useState('');
   const searchParams = useSearchParams();
@@ -17,30 +25,35 @@ export default function Authmercado() {
 
   useEffect(() => {
     if (!code || !nome_loja) {
-      setStatusRequestCodeMercado(false);
+      setModalMessage('Dados de conexão não fornecidos.');
       return;
     }
 
     const fetchData = async () => {
       const userId = searchUserId();
-      if (!userId) return
+      if (!userId) {
+        setModalMessage('Dados de conexão não fornecidos.');
+        return;
+      }
 
       setUserId(userId);
 
       try {
         const res = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/mercadolivre/redirect`, { code, nome_loja, userId });
         if (res.status === 200) {
+          setModalMessage('Conectado com sucesso.');
           setStatusRequestCodeMercado(true);
           router.push('/dashboard');
         } else {
+          setModalMessage('Erro ao processar a solicitação.');
           setStatusRequestCodeMercado(false);
         }
       } catch (error) {
         if (error.response && error.response.status === 409) {
+          setModalMessage('Loja já conectada.');
           setStatusRequestCodeMercado(false);
-          router.push('/dashboard');
         } else {
-          console.error('Erro ao processar a solicitação:', error);
+          setModalMessage('Erro ao processar a solicitação.');
           setStatusRequestCodeMercado(false);
         }
       }
@@ -50,14 +63,34 @@ export default function Authmercado() {
   }, [code, nome_loja, router]);
 
   return (
-    <main className="flex min-h-screen flex-row items-center justify-evenly">
-      <div>
-        <p>Nome da Loja: {nome_loja}</p>
-        <p>Code: {code}</p>
-        <p>UserId: {userId}</p>
+    <main className="flex max-w-full h-screen">
+      <SidebarContent />
+      <div className='w-full px-4 lg:mt-4 xl:mx-8 xl:flex xl:flex-col xl:items-center'>
+        <div className="w-full flex justify-between items-center h-12 pt-5">
+          <div className='flex items-center'>
+            <HamburgerContent />
+            <TitlePage title='Conectando loja do mercado livre' />
+          </div>
+          <ActionsContent />
+        </div>
       </div>
-      {statusRequestCodeMercado === true && <SuccessNotification message="Conectado com sucesso" />}
-      {statusRequestCodeMercado === false && <ErrorNotification message="Erro ao conectar" />}
+
+      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center">
+        <p className="mt-5 text-lg text-neutral-800 dark:text-gray-200">
+          {modalMessage}
+        </p>
+
+        <div className="mt-8 flex justify-center">
+          <Image src='/img/notification/error_bad_request.svg' alt="Erro" className="w-96 h-96" width={384} height={384} />
+        </div>
+        <div className="mt-8 flex justify-center">
+          {statusRequestCodeMercado === true ? (
+            <Image src='/img/notification/sucess_request.svg' alt="Sucesso" className="w-96 h-96" width={384} height={384} />
+          ) : (
+            <Image src='/img/notification/error_bad_request.svg' alt="Erro" className="w-96 h-96" width={384} height={384} />
+          )}
+        </div>
+      </div>
     </main>
   );
 }
